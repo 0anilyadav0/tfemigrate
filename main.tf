b165b71e4204f80a0878459adddf7b71108ea6c2
djenish <87292669+djenish@users.noreply.github.com>
2023-04-19 11:17:21 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,22 @@
+module "create_stg_1" {
+	source= "../"
+	bucket_name= "asedgfhghjk1"
+	}
+
+module "create_stg_2" {
+        source= "../"
+        bucket_name= "asedgfhghjktyuio1"
+        }
+
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.60.0"
+    }
+  }
+}
+
+provider "google" {
+  region      = "asia-south1"
+}
