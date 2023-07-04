b566691d1a168920413b8a4cfe747536c90dfac9
djenish <87292669+djenish@users.noreply.github.com>
2023-07-04 13:32:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,43 +18,15 @@ provider "google" {
   name = "gcp-vpc"
 }*/
 
-resource "google_compute_instance" "default" {
-  name         = "test"
-  machine_type = "e2-medium"
-  zone         = "asia-south1-a"
-
-  tags = ["foo", "bar"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
+resource "google_storage_bucket" "static-site" {
+  name          = "image-store.com"
+  location      = "EU"
+  force_destroy = true
+  labels = {
         my_label = "test_sentinel"
       }
-    }
-  }
 
-  // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
-  }
-
-  network_interface {
-    network = "projects/hbl-uat-host-prj-hub-4d/global/networks/hbl-gcp-sharedsvc-common-uat-vpc-01"
-    subnetwork = "projects/hbl-uat-host-prj-hub-4d/regions/asia-south1/subnetworks/hbl-gcp-sharedsvc-common-uat-as1-infratools-subnet"
-  }
-
-  metadata = {
-    foo = "bar"
-  }
-
-  # metadata_startup_script = "echo hi > /test.txt"
-
-  service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = "tfe-migrate-gcp@hbl-uat-sharedsvc-prj-spk-4d.iam.gserviceaccount.com"
-    scopes = ["cloud-platform"]
-  }
+  uniform_bucket_level_access = true
 }
 
 /*output "gcp_vpc" {
